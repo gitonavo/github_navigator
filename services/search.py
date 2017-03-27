cd04d4b5a06fe12da333859c9d56fed09176ea41
hkb <hkb@tisynch.com>
2017-03-27 10:52:54 +0200

    github navigator added authenticated requests, fetched additional info, decreased per_page results, added error message when getting 403 status code during a commit details request

--- services/search.py
+++ services/search.py
@@ -11,9 +11,13 @@ class SearchService(object):
     @classmethod
     def search_term(cls, term):
 
-        payload = {'q': term, 'sort': 'created', 'order': 'desc', 'per_page': 50}
+        payload = {'q': term, 'sort': 'created', 'order': 'desc', 'per_page': 5}
+        headers = {
+            "Authorization": "Basic Z2l0b25hdm86R2lUME5hdjA=",
+            "Content-Type": "application/json"
+        }
         try:
-            r = requests.get(GITHUB_URL + '/search/repositories', params=payload)
+            r = requests.get(GITHUB_URL + '/search/repositories', params=payload, headers=headers)
         except (requests.Timeout, requests.HTTPError) as he:
             raise HttpError(str(he))
 
@@ -21,17 +25,19 @@ class SearchService(object):
         total_count = rjson.get('total_count', 0)
         items = rjson.get('items', {})
 
-        # for item in items:
-        #     branches_url = item.get('branches_url', '')
-        #     if branches_url:
-        #         branches_url = branches_url[:-9] + '/master'
-        #         master_r = requests.get(branches_url)
-        #         master_rjson = master_r.json()
-        #         commit = master_rjson.get('commit', {})
-        #         item['commit'] = commit
+        for item in items:
+            branches_url = item.get('branches_url', '')
+            if branches_url:
+                branches_url = branches_url[:-9] + '/master'
+                master_r = requests.get(branches_url)
+                if master_r.status_code == 403:
+                    item['commit'] = {'error': master_r.text}
+                else:
+                    master_rjson = master_r.json()
+                    commit = master_rjson.get('commit', {})
+                    item['commit'] = commit
 
         print('Got response {}'.format(rjson))
-        # TODO: call github API, get response and return in template
         m_template = render_template('template.html', search_term=term, total_count=total_count, items=items)
         resp = output_html(m_template, 200)
         return resp
